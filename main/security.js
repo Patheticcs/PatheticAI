67b639f2cbfcbc90f837740ffbac58f51d0cf2ee
path <patheticcs@gmail.com>
2025-02-01 17:14:39 -0800

    Update security.js

--- main/security.js
+++ main/security.js
@@ -1,10 +1,30 @@
 (function() {
+
+    document.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+        alert("DevTools isn't allowed!");
+        window.location.href = 'about:blank'; 
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (
+            e.key === 'F12' || 
+            (e.ctrlKey && e.shiftKey && e.key === 'I') || 
+            (e.ctrlKey && e.shiftKey && e.key === 'J') || 
+            (e.ctrlKey && e.key === 'U')
+        ) {
+            e.preventDefault();
+            alert("DevTools isn't allowed!");
+            window.location.href = 'about:blank'; 
+        }
+    });
+
     const devToolsDetect = () => {
         const threshold = 160;
         const width = window.outerWidth - window.innerWidth > threshold || window.outerHeight - window.innerHeight > threshold;
         if (width) {
             alert("DevTools isn't allowed!");
-            window.location.href = 'about:blank';
+            window.location.href = 'about:blank'; 
         }
     };
 
