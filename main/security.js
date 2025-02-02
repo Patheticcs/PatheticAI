4e6107e4e943ba5211b9d9e637c0aaf95980d109
path <patheticcs@gmail.com>
2025-02-01 17:25:21 -0800

    Update security.js

--- main/security.js
+++ main/security.js
@@ -1,49 +1,53 @@
 (function() {
+    var devToolsOpen = false;
+    var threshold = 160; 
 
-  document.addEventListener('keydown', function(e) {
+    document.addEventListener('keydown', function(e) {
 
-    if (e.keyCode === 123) {
-      e.preventDefault();
-      return false;
-    }
-
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
-      e.preventDefault();
-      return false;
-    }
-
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
-      e.preventDefault();
-      return false;
-    }
-
-    if (e.ctrlKey && e.keyCode === 85) {
-      e.preventDefault();
-      return false;
-    }
-
-  });
-
-  var devToolsOpen = false;
-  var threshold = 160; 
-
-  setInterval(function() {
-
-    var widthDifference = window.outerWidth - window.innerWidth;
-    var heightDifference = window.outerHeight - window.innerHeight;
-
-    if (widthDifference > threshold || heightDifference > threshold) {
-      if (!devToolsOpen) {  
-        devToolsOpen = true;
-
-        if (confirm("Dev Tools Isn't Allowed")) {
-
-          window.location.href = "about:blank";
-
+        if (e.keyCode === 123) {
+            e.preventDefault();
+            return false;
         }
-      }
-    } else {
-      devToolsOpen = false; 
-    }
-  }, 1000); 
+
+        if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+            e.preventDefault();
+            return false;
+        }
+
+        if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
+            e.preventDefault();
+            return false;
+        }
+
+        if (e.ctrlKey && e.keyCode === 85) {
+            e.preventDefault();
+            return false;
+        }
+
+        if (e.ctrlKey) {
+            if (e.key !== 'c' && e.key !== 'v' && e.key !== 'a') {
+                e.preventDefault();
+            }
+        }
+    });
+
+    setInterval(function() {
+        var widthDifference = window.outerWidth - window.innerWidth;
+        var heightDifference = window.outerHeight - window.innerHeight;
+
+        if (widthDifference > threshold || heightDifference > threshold) {
+            if (!devToolsOpen) {  
+                devToolsOpen = true;
+                if (confirm("Dev Tools Isn't Allowed")) {
+                    window.location.href = "about:blank";
+                }
+            }
+        } else {
+            devToolsOpen = false; 
+        }
+    }, 1000);
+
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
 })();
