104f383e2a991bebad5204bd766fb87d2ad89f5c
path <patheticcs@gmail.com>
2025-02-01 18:22:06 -0800

    Update security.js

--- main/security.js
+++ main/security.js
@@ -1,53 +1,54 @@
 (function() {
     var devToolsOpen = false;
-    var threshold = 160; 
+    var threshold = 300;
 
-    document.addEventListener('keydown', function(e) {
-
-        if (e.keyCode === 123) {
-            e.preventDefault();
-            return false;
-        }
-
-        if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
-            e.preventDefault();
-            return false;
-        }
-
-        if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
-            e.preventDefault();
-            return false;
-        }
-
-        if (e.ctrlKey && e.keyCode === 85) {
-            e.preventDefault();
-            return false;
-        }
-
-        if (e.ctrlKey) {
-            if (e.key !== 'c' && e.key !== 'v' && e.key !== 'a') {
+    if (!/Mobi|Android/i.test(navigator.userAgent)) {
+        document.addEventListener('keydown', function(e) {
+            if (e.keyCode === 123) {
                 e.preventDefault();
+                return false;
             }
-        }
-    });
 
-    setInterval(function() {
-        var widthDifference = window.outerWidth - window.innerWidth;
-        var heightDifference = window.outerHeight - window.innerHeight;
+            if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+                e.preventDefault();
+                return false;
+            }
 
-        if (widthDifference > threshold || heightDifference > threshold) {
-            if (!devToolsOpen) {  
-                devToolsOpen = true;
-                if (confirm("Dev Tools Isn't Allowed")) {
-                    window.location.href = "about:blank";
+            if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
+                e.preventDefault();
+                return false;
+            }
+
+            if (e.ctrlKey && e.keyCode === 85) {
+                e.preventDefault();
+                return false;
+            }
+
+            if (e.ctrlKey) {
+                if (e.key !== 'c' && e.key !== 'v' && e.key !== 'a') {
+                    e.preventDefault();
                 }
             }
-        } else {
-            devToolsOpen = false; 
-        }
-    }, 1000);
+        });
 
-    document.addEventListener('contextmenu', function (e) {
-        e.preventDefault();
-    });
+        setInterval(function() {
+            var widthDifference = window.outerWidth - window.innerWidth;
+            var heightDifference = window.outerHeight - window.innerHeight;
+
+            if (widthDifference > threshold || heightDifference > threshold) {
+                if (!devToolsOpen) {
+                    devToolsOpen = true;
+                    if (confirm("Dev Tools Isn't Allowed")) {
+                        window.location.href = "about:blank";
+                    }
+                }
+            } else {
+                devToolsOpen = false;
+            }
+        }, 1000);
+
+        document.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+        });
+    }
 })();
